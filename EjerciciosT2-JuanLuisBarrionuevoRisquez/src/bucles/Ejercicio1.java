9df276bdf0b792b483cfb76fedcb31c86153bfe6
juanl2004 <145675461+juanl2004@users.noreply.github.com>
2023-11-09 13:53:12 +0100

    Ejercicio

--- EjerciciosT2-JuanLuisBarrionuevoRisquez/src/bucles/Ejercicio1.java
+++ EjerciciosT2-JuanLuisBarrionuevoRisquez/src/bucles/Ejercicio1.java
@@ -15,33 +15,68 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 
 		//Declaramos las horas, minutos y segundos que le pediremos al usuario
-		int horas, minutos, segundos;
+		int horas = 1, minutos = 1, segundos = 1;
 		//Declaramos los segundos que le pediremos al usuario que incremente
-		int incremento;
-		//Declaramos los segundos totales
-		int segundosTot;
-		//Declaramos los segundos restantes
-		int segundosRest;
+		int incremento = 0;
+
 
 		//Creamos el Scanner
 		Scanner sc = new Scanner(System.in);
 
-		//Le pedimos al usuario que introduzca las horas, minutos, segundos y los segundos que quiere incrementar
-		System.out.println("Introduce las horas --> ");
-		horas = sc.nextInt();
-
-		System.out.println("Introduce los minutos --> ");
-		minutos = sc.nextInt();
-
-		System.out.println("Introduce los segundos --> ");
-		segundos = sc.nextInt();
-
-		System.out.println("Introduce la cantidad de segundos a incrementar --> ");
-		incremento = sc.nextInt();
-
+		do {
+			try {
+				System.out.println("Introduce las horas --> ");
+				horas = sc.nextInt();	
+			} catch(InputMismatchException e) {
+				System.out.println("El dato introducido no es correcto");
+			}
+		} while(horas < 0 || horas > 23);
 		
+		do {
+			try {
+				System.out.println("Introduce los minutos --> ");
+				minutos = sc.nextInt();	
+			} catch(InputMismatchException e) {
+				System.out.println("El dato introducido no es correcto");
+			}
+		} while(minutos < 0 || minutos > 59);
 		
-		System.out.println("La nueva hora es: " + horas + ":" + minutos + ":" + segundos);
+		do {
+			try {
+				System.out.println("Introduce los segundos --> ");
+				segundos = sc.nextInt();	
+			} catch(InputMismatchException e) {
+				System.out.println("El dato introducido no es correcto");
+			}
+		} while(segundos < 0 || segundos > 59);
+		
+		do {
+			try {
+				System.out.println("Introduce los segundos a incrementar --> ");
+				incremento = sc.nextInt();	
+			} catch(InputMismatchException e) {
+				System.out.println("El dato introducido no es correcto");
+			}
+		} while(segundos < 0 || segundos > 59);
+		
+		for (int i = 0; i < incremento; i++) {
+			segundos++;
+			if (segundos == 60) {
+				segundos = 0; 
+				minutos++;
+			}
+			if (minutos == 60) {
+				minutos = 0;
+				horas++;
+			}
+			if (horas == 24) {
+				horas = 0;
+			}
+			
+			if () {}
+		}
+		
 		sc.close();
-	}
+		
+}
 }
